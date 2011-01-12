4308eb39b128129610044115d4d1ede3950a7a9b
Ryan Cook <ryan@quickleft.com>
2011-01-12 14:05:07 -0700

    only require module mattr_* monkey patch if Module does not respond to mattr_accessor

--- lib/module_ext.rb
+++ lib/module_ext.rb
@@ -1,6 +1,9 @@
 module ModuleExt
 
-  require 'module_ext/attribute_accessors'
+  # Only require if mattr_* methods have not already been added
+  if !Module.respond_to? :mattr_accessor
+    require 'module_ext/attribute_accessors'
+  end
 
 end
 
